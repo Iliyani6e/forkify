c76a4e1fdf2a788d28a397c982b10bad4f7595d7
Iliyan Stefanov <iliqni6e@abv.bg>
2020-01-07 12:38:22 +0200

    Build the search model.

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,5 @@
-import axios from "axios";
+import Search from "./models/Search";
 
-async function getResults(query) {
-  const res = await axios(
-    `https://forkify-api.herokuapp.com/api/search?&q=${query}`
-  );
-  const recipes = res.data.recipes;
-
-  console.log(recipes);
-}
-
-getResults("pizza");
+const search = new Search("pizza");
+console.log(search);
+search.getResults();
