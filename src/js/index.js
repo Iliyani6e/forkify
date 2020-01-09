06e348713297ce9449ffeb75ff54cee227e08573
Iliyan Stefanov <iliqni6e@abv.bg>
2020-01-09 13:38:16 +0200

    Build the searchView for the recipes list.Limit the recipe title to 17 characters.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 //Global state of the app
 // * - Search object
 // * - Current recipe object
@@ -9,20 +11,22 @@ const state = {};
 
 const controllSearch = async () => {
   // 1. Get the query from the view
-  const query = "pizza"; //TODO
+  const query = searchView.getInput(); //TODO
+  console.log(query);
   if (query) {
     //2.New search object add to state
     state.search = new Search(query);
     //3. Prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
     //4.Search for recepis
     await state.search.getResults();
     //5.Render the results in the UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector(".search").addEventListener("submit", e => {
+elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controllSearch();
 });
